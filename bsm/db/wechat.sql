b9f1dd4180b474e2aa3e7138aab6b7b33e8e8e21
dreamshield <zhpfcn@gmail.com>
2015-02-06 18:44:52 +0800

    完成登陆界面设计

--- bsm/db/wechat.sql
+++ bsm/db/wechat.sql
@@ -2,14 +2,21 @@
 CREATE DATABASE wechat;
 SET NAMES utf8;
 USE wechat;
-CREATE TABLE userMsg
-(
+
+-- 用户消息表
+CREATE TABLE userMsg(
 	msgid CHAR(9) NOT NULL PRIMARY KEY,
 	fakeid CHAR(10) NOT NULL,
 	nickname CHAR(64) NOT NULL,
 	content VARCHAR(255),
 	time INT UNSIGNED,
 	audit TINYINT
+) DEFAULT CHARSET utf8  COLLATE utf8_general_ci;
+
+-- 管理员账号
+CREATE TABLE admin(
+	username CHAR(16) NOT NULL PRIMARY KEY,
+	password CHAR(40) NOT NULL
 ) DEFAULT CHARSET utf8  COLLATE utf8_general_ci;
 
 -- 用户授权
