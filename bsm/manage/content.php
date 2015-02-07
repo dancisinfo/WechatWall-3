a7bfd422353dcd4180f47d02dae06f681ae9374e
dreamshield <zhpfcn@gmail.com>
2015-02-07 20:35:39 +0800

    后台消息管理完成

--- bsm/manage/content.php
+++ bsm/manage/content.php
@@ -20,53 +20,66 @@ if (isset($_SESSION['userid'])) {
 <?php
 	$userid = $_SESSION['userid'];
 	if (isset($_GET['menu']) && ('audited' == $_GET['menu'])) {
-		$menu = $_GET['menu'];
-		echo "这里是审核通过的消息";
+		$db = new mysqlDB($localhost, $userDB, $pwdDB, $database);
+		$query = "SELECT msgid, nickname, fakeid, time, content FROM userMsg WHERE audit = '1'";
+		$auditedMsg = $db->find($query);
+		$db->close();
+		if (NULL != $auditedMsg) {
+			$table = "<table><tr>";
+			$table .= "<th>消息ID</th><th>昵称</th><th>FakeID</th><th>时间</th><th>内容</th></tr>";
+			foreach ($auditedMsg as $msg) {
+				$table .= "<tr>";
+				foreach ($msg as $value) {
+					$table .="<td>$value</td>";
+				}
+				$table .= "</tr>";
+			}
+			$table .= "</table>";
+			echo $table;
+		} else {
+			echo "<meta charset='utf-8'>";
+			echo "没有消息!";
+		}
 	} else {
 		// 将爬取的最新用户消息写入数据库
-		// $wechat = new wechat($userWechat, $pwdWechat);
-		// $wechat->msgGetAndStore($localhost, $userDB, $pwdDB, $database);
+		$wechat = new wechat($userWechat, $pwdWechat);
+		$wechat->msgGetAndStore($localhost, $userDB, $pwdDB, $database);
 		// 从wechat数据库中读取还没有处理的消息,准备上墙
 		$db = new mysqlDB($localhost, $userDB, $pwdDB, $database);
-		$query = "SELECT nickname, fakeid, time, content FROM userMsg WHERE audit = '0'";
+		$query = "SELECT msgid, nickname, fakeid, time, content FROM userMsg WHERE audit = '0'";
 		$newMsg = $db->find($query);
 		$db->close();
 		if (NULL != $newMsg) {
 			$table = "<table><tr>";
-			$table .= "<th>昵称</th><th>FakeID</th><th>时间</th><th>内容</th><th>上墙</th></tr>";
+			$table .= "<th>消息ID</th><th>昵称</th><th>FakeID</th><th>时间</th><th>内容</th><th>上墙</th></tr>";
 			foreach ($newMsg as $msg) {
 				$table .= "<tr>";
 				foreach ($msg as $value) {
 					$table .="<td>$value</td>";
 				}
-				// 这里对应表格里 上墙,操作,占位
-				$table .= "<td><form action='xxx.php'>";
+				// 这里对应表格里 上墙,操作
+				$table .= "<td><form method='post' action='msgAudit.php?msgid=".$msg['msgid']."'>";
 				$table .= "<div id='select'>";
-				$table .= "<input type='radio' name='op' value='Y'>&nbsp;Y<br>";
+				$table .= "<input type='radio' name='op' value='Y' checked>&nbsp;Y<br>";
 				$table .= "<input type='radio' name='op' value='N'>N";
 				$table .= "</div><div id='submit'>";
 				$table .= "<input type='Submit' name='sub' value='执行'>";
 				$table .="</div></form></td>";
 				$table .= "</tr>";
 			}
+			$table .= "</table>";
 			echo $table;
+		} else {
+			echo "<meta charset='utf-8'>";
+			echo "没有新消息!";
 		}
-?>
-
-</table>
-<?php
 	}
-
 ?>
-
 </body>
 </html>
-
-
 <?php
 } else {
 	echo "<meta charset='utf-8'>";
 	echo "您没有权限访问此页面,请同管理员联系!";
 }
-
 ?>
---
+++ bsm/manage/msgAudit.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * FileName: msgAudit.php
+ * Discription: 用户消息审核结果入库
+ * ModifyHistory:
+ * 1. 2015-02-07    19:40    Dreamshield
+ * 创建源文件
+ */
+require_once('/var/www/project/WechatWall/bsm/inc/main.inc.php');
+session_start();
+if (isset($_SESSION['userid'])) {
+	$db = new mysqlDB($localhost, $userDB, $pwdDB, $database);
+	$msgid = $_GET['msgid'];
+	$op = $_POST['op'];
+	if ('Y' == $op) {
+		$audit = array('audit'=>1);
+		$where = "msgid = '$msgid'";
+		$db->update('userMsg', $audit, $where);
+	}
+	if ('N' == $op) {
+		$where = "msgid = '$msgid'";
+		$db->delete('userMsg', $where);
+	}
+	$db->close();
+	header('Location:content.php');
+} else {
+	echo "<meta charset='utf-8'>";
+	echo "您没有权限访问此页面,请同管理员联系!";
+}
